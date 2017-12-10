beaaa7f048d02a41c57c7b4c4423cdc5572d0db3
Tiffany Chen <tiffanysta.c@gmail.com>
2017-12-10 18:22:46 -0500

    finished assignment

--- dom.js
+++ dom.js
@@ -1,12 +1,40 @@
 var b = document.getElementById("b");
+var num = 8;
 
-b.addEventListener('click', callback) {
-
-};
-
-var callback = function(e) {
-	var l = document.getElementById("thelist");
+var callbackButton = function(e) {
+	//console.log("This ran.");
+	var list = document.getElementById("thelist");
 	var newDiv = document.createElement("li");
-	newDiv.innerHTML = "item";
-	l.appendChild(newDiv);
+	newDiv.innerHTML = "item " + num;
+	num ++;
+	element = list.appendChild(newDiv);
+	element.addEventListener('mouseover', callbackHover);
+	element.addEventListener('mouseleave', callbackLeave);
+	element.addEventListener('click', callbackRemove);
 };
+
+b.addEventListener('click', callbackButton);
+
+var list = document.getElementsByTagName("li");
+
+var callbackHover = function(e) {
+	var heading = document.getElementById("h");
+	heading.innerHTML = this.innerHTML;
+}
+
+var callbackLeave = function(e) {
+	var heading = document.getElementById("h");
+	heading.innerHTML = "Hello World!";
+}
+
+var callbackRemove = function(e) {
+	var list = document.getElementById("thelist");
+	list.removeChild(this);
+}
+
+for (i = 0; i < list.length; i ++) {
+	element = list[i];
+	element.addEventListener('mouseover', callbackHover);
+	element.addEventListener('mouseleave', callbackLeave);
+	element.addEventListener('click', callbackRemove);
+}
