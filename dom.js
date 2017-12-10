a1e85475471ef4273400870f0beb5efc84f86491
doniatung <doniatung32@gmail.com>
2017-12-09 19:16:42 -0500

    the button part works, not sure what's up with my computer w the variables though

--- dom.js
+++ dom.js
@@ -1,12 +1,29 @@
 var b = document.getElementById("b");
+var list = document.getElementsByTagName("li");
+var heading = document.getElementById("h");
 
-b.addEventListener('click', callback) {
-
+var buttonCallback = function(e) {
+    var l = document.getElementById("thelist");
+    var newDiv = document.createElement("li");
+    newDiv.innerHTML = "new item";
+    l.appendChild(newDiv);
 };
 
-var callback = function(e) {
-	var l = document.getElementById("thelist");
-	var newDiv = document.createElement("li");
-	newDiv.innerHTML = "item";
-	l.appendChild(newDiv);
+b.addEventListener('click', buttonCallback);
+
+
+var listOverCallback = function(e){
+    heading.innerHTML = this.innerHTML;
 };
+
+var listLeaveCallback = function(e){
+    heading.innerHTML = "Hello World!";
+}
+		   
+var listClickCallback = function(e){
+    this.remove();
+};
+
+list.addEventListener("mouseenter", listOverCallback);
+list.addEventListener("mouseleave", listLeaveCallback);
+list.addEventListener("click", listClickCallback);
